b09c81c5bba93fd56e3bd88657fbabae15cfcac4
zhengzhaoxiang <1059311525@qq.com>
2020-06-08 23:54:15 +0800

    泛型 p4 添加 yzs 遇到的泛型返回多种类型的方法

--- Generic/src/com/basicjava/generic/p4/Test4.java
+++ Generic/src/com/basicjava/generic/p4/Test4.java
@@ -1,10 +1,18 @@
 package com.basicjava.generic.p4;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.Random;
+
 // 泛型方法
 public class Test4 {
 
     public static void main(String[] args) {
 
+        System.out.println(func3().t.getClass());
+        System.out.println(func4().t.getClass());
+
+        Integer i = (Integer) new Object();
+        System.out.println(i.getClass());
     }
 
     // 1. 方法泛型是指作用在方法上的 T，后面 返回值，入参，入参的泛型等地方都能使用这个 T
@@ -13,9 +21,52 @@ public class Test4 {
         return t;
     }
 
+    public static <E> Generate<E> func1() {
+
+        if (new Random().nextBoolean())
+            return (Generate<E>) new Generate<Integer>();
+        else
+            return (Generate<E>) new Generate<String>();
+
+    }
+
+    public static Generate func2() {
+
+        if (new Random().nextBoolean())
+            return (Generate) new Generate<Integer>();
+        else
+            return (Generate) new Generate<String>();
+
+    }
+
+    public static Generate<?> func3() {
+
+        if (new Random().nextBoolean())
+            return new Generate<Integer>();
+        else
+            return new Generate<String>();
+
+    }
+
+    public static Generate func4() {
+
+        if (new Random().nextBoolean())
+            return new Generate<Integer>();
+        else
+            return new Generate<String>();
+
+    }
+
 }
 
 class Generate<T> {
+
+    T t = (T) new Object();
+
+    public Class<T> getTClass() {  // https://blog.csdn.net/hellozhxy/article/details/82024712
+        Class<T> tClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        return tClass;
+    }
 
     private <E> void func1(E t) {
 
