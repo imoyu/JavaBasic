8a493d0986019f575b539b8993e7ea128646f43f
zhengzhaoxiang <1059311525@qq.com>
2020-07-24 19:50:02 +0800

    正则 test

--- SomeTest/src/test/java/com/test/T1.java
+++ SomeTest/src/test/java/com/test/T1.java
@@ -3,6 +3,8 @@ package com.test;
 import org.junit.Test;
 
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class T1 {
 
@@ -34,4 +36,35 @@ public class T1 {
 
     }
 
+    @Test
+    public void testContinue() {
+
+        Boolean f1 = true;
+        Boolean f2 = true;
+        Boolean f3 = true;
+
+        for (int i = 0; i < 10; i++) {
+            if (f1 != null) {
+                if (f2 != null) {
+                    if (f3) {
+                        continue;
+                    }
+                }
+            }
+            System.out.println("=================>");
+        }
+        System.out.println(String.format("finish%d", 12L));
+    }
+
+    @Test
+    public void testRegex() {
+        Pattern pattern = Pattern.compile("[a-z]\\d");
+        Matcher matcher = pattern.matcher("ess231r2gfd4f");
+
+        // matcher使用matches()方法后就失效了，不能再使用find取值：
+        System.out.println(matcher.matches());         // --> false
+
+//        matcher.find();
+        System.out.println(matcher.group(0));      // --> s2   r2    d4
+    }
 }
