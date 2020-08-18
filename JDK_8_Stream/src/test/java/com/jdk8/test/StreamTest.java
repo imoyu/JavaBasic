f16cef9edc06c2da3afde1f0c9a95b1322b2ec6c
zhengzhaoxiang <1059311525@qq.com>
2020-08-18 10:53:34 +0800

    stram.map 调用方法

--- JDK_8_Stream/src/test/java/com/jdk8/test/StreamTest.java
+++ JDK_8_Stream/src/test/java/com/jdk8/test/StreamTest.java
@@ -20,6 +20,37 @@ public class StreamTest {
         List<String> list2 = Arrays.asList("Djkl", "Hulo", "Chi");
         String s2 = list2.stream().map(String::toUpperCase).collect(Collectors.joining(",", "(", ")"));
         System.out.printf("%s%n%s", s2, list2.get(0));
+        list2.stream().map(StreamTest::func).forEach(System.out::println);
+
+        List<WrapString> list3 = Arrays.asList(new WrapString("Hihh"), new WrapString("Kokook"));
+        list3.stream().map(WrapString::func).forEach(System.out::println);
+
     }
 
+    private static String func(String src) {
+        return src.trim().substring(1, src.length());
+    }
+
+    protected class WrapString {
+        private String str;
+
+        public WrapString(String str) {
+            this.str = str;
+        }
+
+        public WrapString() {
+        }
+
+        public String func() {
+            return str.trim().substring(1, str.length());
+        }
+
+        public String getStr() {
+            return str;
+        }
+
+        public void setStr(String str) {
+            this.str = str;
+        }
+    }
 }
