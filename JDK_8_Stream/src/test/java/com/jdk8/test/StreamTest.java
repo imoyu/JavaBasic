9bdfb4bf1206f265cb1e3325a2ab41a0c2ed496f
zhengzhaoxiang <1059311525@qq.com>
2020-08-18 10:01:23 +0800

    stream.collect(Collectors.joining(";") 方法

--- JDK_8_Stream/src/test/java/com/jdk8/test/StreamTest.java
+++ JDK_8_Stream/src/test/java/com/jdk8/test/StreamTest.java
@@ -3,7 +3,9 @@ package com.jdk8.test;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamTest {
 
@@ -13,7 +15,11 @@ public class StreamTest {
         List<String> list = new ArrayList<>();
         String s = list.stream().reduce((s1, s2) -> s1 + ";" + s2).orElse("null");
         System.out.println(s);
-
+        String s1 = list.stream().collect(Collectors.joining(";"));
+        System.out.println(s1.equals("")); // true
+        List<String> list2 = Arrays.asList("Djkl", "Hulo", "Chi");
+        String s2 = list2.stream().map(String::toUpperCase).collect(Collectors.joining(",", "(", ")"));
+        System.out.printf("%s%n%s", s2, list2.get(0));
     }
 
 }
