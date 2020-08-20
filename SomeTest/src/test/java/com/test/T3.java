ad3b14a3920d19395e8422ac5865a643c3d291d6
zhengzhaoxiang <1059311525@qq.com>
2020-08-20 09:46:37 +0800

    SomeTest double test

---
+++ SomeTest/src/test/java/com/test/T3.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.junit.Test;
+
+public class T3 {
+
+    @Test
+    public void doubleTest() {
+
+        double d1 = 1.25;
+        double d2 = 1.5;
+        double d3 = 1.1;
+        double d4 = 1.2;
+
+        System.out.println(d1 + d2);
+        System.out.println(d3 + d4);
+        System.out.println(d1 + d4);
+        System.out.println(0.01 + 0.05);
+
+    }
+
+}
