e6b26bb98e5470758d9973b3c6c931120d084282
zhengzhaoxiang <1059311525@qq.com>
2020-08-30 23:28:11 +0800

    module JDK_8_stream S1StreamIntroduction update

--- JDK_8_Stream/src/main/java/com/stream/baeldung/stream/S1StreamIntroduction.java
+++ JDK_8_Stream/src/main/java/com/stream/baeldung/stream/S1StreamIntroduction.java
@@ -3,6 +3,7 @@ package com.stream.baeldung.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class S1StreamIntroduction {
@@ -28,6 +29,23 @@ public class S1StreamIntroduction {
                 return detail.getParts().stream();
             }
         };
+        Stream.of("").allMatch(new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.contains("look");
+            }
+        });
+        // 一个类没有重写 equals，就会调用父类的 equals
+        // 如果父类重写了 equals 和 hashcode，子类没有，调用 equals 相当于比较父类
+        PP pp1 = new PP();
+        pp1.j = 10;
+        PP pp2 = new PP();
+        pp2.j = 11;
+        System.out.println(pp1.equals(pp2));
+
+//        Stream.of("", "f").collect();
+//        Stream.of(1, 2).collect();
+//        Stream.of(new Object()).collect();
     }
 
 }
@@ -38,4 +56,27 @@ class Detail {
     public List<String> getParts() {
         return parts;
     }
+}
+
+class P {
+    int i;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        P p = (P) o;
+
+        return i == p.i;
+    }
+
+    @Override
+    public int hashCode() {
+        return i;
+    }
+}
+
+class PP extends P {
+    int j;
 }
