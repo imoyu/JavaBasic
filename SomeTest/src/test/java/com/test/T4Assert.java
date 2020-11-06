3fc22f8bb7327186c8a9d60b541d84d283d60ab5
zhengzhaoxiang <1059311525@qq.com>
2020-11-06 11:32:37 +0800

    assert

---
+++ SomeTest/src/test/java/com/test/T4Assert.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Test;
+
+public class T4Assert {
+
+    @Test
+    public void assertTest() {
+        boolean isOpen = false;
+
+        // 如果开启了断言，会将isOpen的值改为true
+        assert isOpen = true;
+
+        // 打印是否开启了断言，如果为false，则没有启用断言
+        System.out.println(isOpen);
+    }
+
+}
