0edac0d5177360416c049a7cc7af0a7067d2e364
zhengzhaoxiang <1059311525@qq.com>
2021-04-30 16:49:18 +0800

    异常堆栈信息打印

--- LogBackTest/src/main/java/com/log/test/InitCauseException.java
+++ LogBackTest/src/main/java/com/log/test/InitCauseException.java
@@ -1,21 +1,47 @@
 package com.log.test;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.io.IOException;
+
 public class InitCauseException {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
 
-        f1();
+        try {
+            f2();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+            System.out.println(e.getCause());
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getSuppressed());
+            System.out.println(ExceptionUtils.getStackTrace(e));
+        }
 
     }
 
-    private static void f1() throws Exception{
+    private static void f1() {
         try {
-            throw new NullPointerException();
-        } catch (NullPointerException e) {
-            Exception ex = new Exception();
-            ex.initCause(e);
-            throw e;
+            throw new IOException("IO 异常");
+        } catch (IOException e) {
+            throw new RuntimeException("包装异常", e);
         }
     }
+
+    private static void f2() {
+        f1();
+    }
+    private static void f3() {
+        throw new RuntimeException("f3 发生异常");
+    }
+    private static void f4() {
+        f3();
+    }
+    private static void f5() {
+        f4();
+    }
+
 
 }
